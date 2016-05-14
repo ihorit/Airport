fc2e81cd7d41cc172c274b7d52f887b63202622a
ihorit <ihor.tymkiv.it@gmail.com>
2016-05-14 20:15:22 +0300

    create controller for adding admin users

---
+++ airport/admintab/NewUserController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airport.admintab;
+
+import javafx.fxml.FXML;
+
+/**
+ *
+ * @author it01
+ */
+public class NewUserController {
+    @FXML
+    private String login;
+    @FXML
+    private String firtName;
+    @FXML
+    private String secondName;
+    @FXML
+    private String password;
+    
+}
