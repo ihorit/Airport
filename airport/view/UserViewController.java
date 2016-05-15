0f9784078981311db665ab6a3fffc12f160a5eb4
ihorit <ihor.tymkiv.it@gmail.com>
2016-05-15 16:38:37 +0300

    hm

--- airport/view/UserViewController.java
+++ airport/view/UserViewController.java
@@ -61,7 +61,7 @@ public class UserViewController {
         {
             final String DB_URL = "jdbc:mysql://localhost:3306/airport";
             final String USER_NAME = "root";
-            final String PASSWORD = "ae201c8c";
+            final String PASSWORD = "password";
             
             Connection connection = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
             
